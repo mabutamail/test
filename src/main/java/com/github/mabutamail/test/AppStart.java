e82957387ce019136938a991d8c8fdda7f2357e6
mabuta <mabutamail@gmail.com>
2020-11-25 18:53:44 +0300

    add JUnit5

--- src/main/java/com/github/mabutamail/test/AppStart.java
+++ src/main/java/com/github/mabutamail/test/AppStart.java
@@ -5,8 +5,11 @@ import org.slf4j.LoggerFactory;
 
 public class AppStart {
     public final static Logger LOGGER = LoggerFactory.getLogger(AppStart.class);
+
     public static void main(String[] args) {
         LOGGER.info("====================       Начало программы        ====================");
-        System.out.println("Программа работает");
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(12, 67));
     }
+
 }
---
+++ src/main/java/com/github/mabutamail/test/Calculator.java
@@ -0,0 +1,9 @@
+package com.github.mabutamail.test;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
